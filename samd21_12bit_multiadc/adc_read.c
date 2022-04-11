eaa01aa81dabcc2ce3fb016e0960e42d4e6ce14f
gulkanguner <gulkanguner@gmail.com>
2022-04-12 01:21:55 +0300

    code design arrangement and small fixings

--- samd21_12bit_multiadc/adc_read.c
+++ samd21_12bit_multiadc/adc_read.c
@@ -5,28 +5,43 @@
  *  Author: SOFTWARE
  */ 
 
+#include <string.h>
+
+#define ADC_VREF 3300 //mV
+#define ADC_MAX_BIT 4095 //12bit ADC
+#define ADC_BUFFER_SIZE 2 //ADC read buffer size
 
 #include <atmel_start.h>
 #include "adc_read.h"
 
-void adc_enable()
-{
+void adc_enable() {
 	
 	adc_sync_enable_channel(&ADC_0,0);
 	adc_sync_enable_channel(&ADC_0,1);
+	
+	return;
 }
 
-int ADC_Read(ADC_ReadTypeDef_t * ADC_ReadTypeDef)
-{
-	int ret=adc_sync_read_channel(&ADC_0,0,ADC_ReadTypeDef->buffer,2);
-	int ret1=adc_sync_read_channel(&ADC_0,1,ADC_ReadTypeDef->buffer2,2);
-	if(ret<=0&&ret1<=0)
-	{
+int ADC_Read(ADC_ReadTypeDef_t * ADC_ReadTypeDef) {
+	
+	uint8_t buffer[ADC_BUFFER_SIZE] = {0};
 		
+	int read_size = adc_sync_read_channel(&ADC_0, 0, buffer, ADC_BUFFER_SIZE); // Read Channel Number 0
+	if(read_size != 2) {		
 		return -1;
 	}
-	 ADC_ReadTypeDef->PA06_ADC_value = (ADC_ReadTypeDef->buffer[1]  + ADC_ReadTypeDef->buffer[0]);
-	ADC_ReadTypeDef->PA07_ADC_value = (ADC_ReadTypeDef->buffer2[1]  + ADC_ReadTypeDef->buffer2[0]);
-	//ADC_ReadTypeDef->sum = ADC_ReadTypeDef->PA06_ADC_value;
+	memcpy(&ADC_ReadTypeDef->PA06_ADC_value, &buffer[0], ADC_BUFFER_SIZE);
+	ADC_ReadTypeDef->PA06_voltage = ADC_ReadTypeDef->PA06_ADC_value*ADC_VREF/ADC_MAX_BIT;
+
+////
+
+	read_size = adc_sync_read_channel(&ADC_0, 1, buffer, ADC_BUFFER_SIZE); // Read Channel Number 1
+	if(read_size != 2) {
+		return -1;
+	}
+	memcpy(&ADC_ReadTypeDef->PA07_ADC_value, &buffer[0], ADC_BUFFER_SIZE);
+	ADC_ReadTypeDef->PA07_voltage = ADC_ReadTypeDef->PA07_ADC_value*ADC_VREF/ADC_MAX_BIT;
+	
+
 	return 0;
 }
--- samd21_12bit_multiadc/main.c
+++ samd21_12bit_multiadc/main.c
@@ -10,24 +10,26 @@ int main(void)
 	/* Replace with your application code */
 	adc_enable();
 
-	while (1)
-	{
+	while (1) {
+		
 		ADC_ReadTypeDef_t  adc_read;
-		ret=ADC_Read(&adc_read);
-		if(ret==0){
+		ret = ADC_Read(&adc_read);
+		if (ret == 0) {
 			//float PA06_ADC_volts = (adc_read.PA06_ADC_value * 5.0) / 4.095;
 			//float PA07_ADC_volts = (adc_read.PA07_ADC_value * 5.0) / 4.095;
 			//printf("PA06volts=%0.2f\r\n",PA06_ADC_volts);
 			//printf("PA07volst=%0.2f\r\n",PA07_ADC_volts);
-			printf("PA06=%03d\r\n",adc_read.PA06_ADC_value);
-			printf("PA07=%03d\r\n",adc_read.PA07_ADC_value);
-			delay_ms(250);
-			
+			printf("PA06 raw     = %03d\r\n",adc_read.PA06_ADC_value);
+			printf("PA07 raw     = %03d\r\n",adc_read.PA07_ADC_value);
+			printf("PA06 voltage = %03d\r\n",(int)adc_read.PA06_voltage);
+			printf("PA07 voltage = %03d\r\n",(int)adc_read.PA07_voltage);
+			printf("----- ------------- -----\r\n");
+
+			delay_ms(1000);
 		}
-		else
-		{
-			printf("Okuma Basarisiz");
+		else {
+			printf("Reading Failed!");
 		}
-		delay_ms(250);
+		delay_ms(500);
 	}
 }

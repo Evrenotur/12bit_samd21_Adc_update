041f48f8b04a22f276c8f8e1d84860e17e876ce9
Evrenotur <101205887+Evrenotur@users.noreply.github.com>
2022-07-05 20:30:49 +0300

    Samd21_12bit_update

---
+++ samd21_12bit_multiadc/samd21_12bit_multiadc/adc_read.c
@@ -0,0 +1,72 @@
+/*
+ * adc_read.c
+ *
+ * Created: 6.04.2022 22:15:27
+ *  Author: SOFTWARE
+ */ 
+
+#include <string.h>
+
+#define ADC_VREF 3300 //mV
+#define ADC_MAX_BIT 4095 //12bit ADC
+#define ADC_BUFFER_SIZE 2 //ADC read buffer size
+
+#include <atmel_start.h>
+#include "adc_read.h"
+        
+		
+		 /* adc_sync_enable_channel (struct adc_sync_descriptor *const descr, const uint8_t
+             channel)
+             */
+void adc_enable() {
+	
+	adc_sync_enable_channel(&ADC_0,0);
+	adc_sync_enable_channel(&ADC_0,1);
+	
+	return;
+}
+/* Adc Read Pin Number PA06-PA07
+
+int32_t adc_sync_read_channel(struct adc_sync_descriptor *const descr, const uint8_t
+channel, uint8_t *const buffer,const uint16_t length)
+
+If the read is successful, the ADC_Read function returns 0, if it fails, it returns -1.
+*/
+int ADC_Read(ADC_ReadTypeDef_t * ADC_ReadTypeDef) {
+	
+	uint8_t buffer[ADC_BUFFER_SIZE] = {0};
+	/* Read Channel Number 0 */
+	
+	int read_size = adc_sync_read_channel(&ADC_0, 0, buffer, ADC_BUFFER_SIZE); 
+	if(read_size != 2) {
+		return -1;
+	}
+	/* Converting the value read from the GPIO PA06 pin to voltage */
+	memcpy(&ADC_ReadTypeDef->PA06_ADC_value, &buffer[0], ADC_BUFFER_SIZE);
+	
+
+	ADC_ReadTypeDef->PA06_voltage = ADC_ReadTypeDef->PA06_ADC_value*ADC_VREF/ADC_MAX_BIT;
+
+	////
+      /* Read Channel Number 1 */
+	read_size = adc_sync_read_channel(&ADC_0, 1, buffer, ADC_BUFFER_SIZE); 
+	if(read_size != 2) {
+		return -1;
+		
+	}
+	/* Converting the value read from the GPIO PA07 pin to voltage */
+	memcpy(&ADC_ReadTypeDef->PA07_ADC_value, &buffer[0], ADC_BUFFER_SIZE);
+	
+	ADC_ReadTypeDef->PA07_voltage = ADC_ReadTypeDef->PA07_ADC_value*ADC_VREF/ADC_MAX_BIT;
+	 /*The process of summing and averaging the voltage value read*/
+	 	for(int i=0;i<1023;i++)
+	 	{
+		 	
+		 ADC_ReadTypeDef->read_value_1 += ADC_ReadTypeDef->PA07_ADC_value;
+		 	ADC_ReadTypeDef->read_value_2 += ADC_ReadTypeDef->PA06_ADC_value;
+	 	}
+	 	 ADC_ReadTypeDef->ort_1= ADC_ReadTypeDef->read_value_1 /1023;
+	 	 ADC_ReadTypeDef->ort_2= ADC_ReadTypeDef->read_value_2 /1023;
+
+	return 0;
+}
--- samd21_12bit_multiadc/samd21_12bit_multiadc/main.c
+++ samd21_12bit_multiadc/samd21_12bit_multiadc/main.c
@@ -1,5 +1,7 @@
 #include <atmel_start.h>
 #include "adc_read.h"
+#include <math.h>
+
 int main(void)
 {
 	int ret=0;
@@ -7,6 +9,7 @@ int main(void)
 	int read_value_2=0;
 	int snc_1=0;
 	int snc_2=0;
+	
 	
 	/* Initializes MCU, drivers and middleware */
 	atmel_start_init();
@@ -28,31 +31,22 @@ read_value_2 -> Variable that holds the average of the voltage read from the GPI
 		ADC_ReadTypeDef_t  adc_read;
 		ret = ADC_Read(&adc_read);
 		if (ret == 0) {
-			//float PA06_ADC_volts = (adc_read.PA06_ADC_value * 5.0) / 4.095;
-			//float PA07_ADC_volts = (adc_read.PA07_ADC_value * 5.0) / 4.095;
-			//printf("PA06volts=%0.2f\r\n",PA06_ADC_volts);
-			//printf("PA07volst=%0.2f\r\n",PA07_ADC_volts);
+
+
 			printf("PA06 raw     = %03d\r\n",adc_read.PA06_ADC_value);
 			printf("PA07 raw     = %03d\r\n",adc_read.PA07_ADC_value);
 			printf("PA06 voltage = %03d\r\n",(int)adc_read.PA06_voltage);
 			printf("PA07 voltage = %03d\r\n",(int)adc_read.PA07_voltage);
+			printf("sn1c=%d,snc2=%d\r\n",adc_read.ort_1,adc_read.ort_2);
 			printf("----- ------------- -----\r\n");
 			
 		/*The process of summing and averaging the voltage value read*/
-			for(int i=0;i<1023;i++)
-			{
+				
 		
-				read_value_1 += adc_read.PA06_voltage;
-					read_value_2 += adc_read.PA07_voltage;
-			}
-             snc_1 = read_value_1 /1023;
-			 snc_2 = read_value_2 /1023;
-			 printf("snc_1 = %03d\r\n",(int)snc_1);
-			 printf("snc_2 = %03d\r\n",(int)snc_2);
-			 
-			 
+		 
 			
 
+                
 
 			delay_ms(1000);
 		}
